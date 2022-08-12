30dc8f4f75b12e57732b3d08f451ce486fafb43c
unknown <a.kiaipoor@gmail.com>
2022-08-12 13:03:51 +0430

    SQL

---
+++ SQL/1.sql
@@ -0,0 +1,9 @@
+-- CUSTOMERS INSERT
+INSERT INTO customers(name,city,country) VALUES('ali','mashhad','iran');
+INSERT INTO customers(name,city,country) VALUES('reza','tehran','iran');
+INSERT INTO customers(name,city,country) VALUES('alireza','mashhad','iran');
+INSERT INTO customers(name,city,country) VALUES ('hasan','LA','usa');
+-- PRODUCTS INSERT
+INSERT INTO products(name,price,count) VALUES('mouse','250000','20');
+INSERT INTO products(name,price,count) VALUES('battery','25000','50');
+INSERT INTO products(name,price,count) VALUES('pen','10000','100');
---
+++ SQL/5.sql
@@ -0,0 +1,1 @@
+UPDATE products SET price = price - 0.2*price;
